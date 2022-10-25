10e2db05f3fdd6af3405aa9f46bc3dd63b13ad6d
VanyEly <99744796+VanyEly@users.noreply.github.com>
2022-10-25 19:39:36 -0300

    Mi primer commit en heroku

---
+++ src/main/java/com/portfolio/bve/Service/ImpPersonaService.java
@@ -0,0 +1,38 @@
+package com.portfolio.bve.Service;
+
+import com.portfolio.bve.Entity.Persona;
+import com.portfolio.bve.Interface.IPersonaService;
+import com.portfolio.bve.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService{
+@Autowired IPersonaRepository ipersonaRepository;
+
+            
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id).orElse(null);
+        return persona;
+    }
+    
+}
+
